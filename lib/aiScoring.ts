96ebf343cd99d775522317028a1561395e84f6ce
OHM-Patchamet <patchamet.sriaksorn@unicity.com>
2025-04-24 15:23:00 +0700

    apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,

--- lib/aiScoring.ts
+++ lib/aiScoring.ts
@@ -13,7 +13,7 @@ export interface AIAnalysisResult {
 }
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
   dangerouslyAllowBrowser: true
 });
 
